3a438297a04668d4402f82bca78d48deede6173a
firuz2006 <firuzisoboev@gmail.com>
2024-01-17 16:12:50 +0500

    forget "Title"

--- src/MRA.Pages.Domain/Entities/Content.cs
+++ src/MRA.Pages.Domain/Entities/Content.cs
@@ -3,6 +3,7 @@ namespace MRA.Pages.Domain.Entities;
 public class Content: BaseEntity
 {
     public string HtmlContent { get; set; } = "";
+    public required string Title { get; set; }
     public required string Lang { get; set; }
     public Guid PageId { get; set; }
     public Page? Page { get; set; }
