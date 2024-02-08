21d98d7618d4450f8708fea8d54e595e69da5f33
firuz2006 <firuzisoboev@gmail.com>
2024-02-08 16:44:47 +0500

    /pages/ prefix

--- src/MRA.Pages.Api/DependencyInitializer.cs
+++ src/MRA.Pages.Api/DependencyInitializer.cs
@@ -1,14 +1,30 @@
+using MRA.Pages.Api.Controllers;
 using MRA.Pages.Api.Filters;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MRA.Pages.Api;
 
 public static class DependencyInitializer
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, bool isDevelopmentMode)
     {
         services.AddControllersWithViews();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        if (isDevelopmentMode)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    if (apiDesc.TryGetMethodInfo(out var methodInfo))
+                    {
+                        return methodInfo.DeclaringType?.Name == nameof(PagesController);
+                    }
+
+                    return false;
+                });
+            });
+        }
 
         services.AddControllers(opt => { opt.Filters.Add<ApiExceptionFilter>(); });
         return services;
