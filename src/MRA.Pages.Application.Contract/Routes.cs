55f73584bf1d014605cc75a8862b686af43929f5
firuz2006 <firuzisoboev@gmail.com>
2024-02-09 13:29:02 +0500

    fix tests

--- src/MRA.Pages.Application.Contract/Routes.cs
+++ src/MRA.Pages.Application.Contract/Routes.cs
@@ -2,7 +2,7 @@ namespace MRA.Pages.Application.Contract;
 
 public static class Routes
 {
-    private const string Base = "/api";
+    private const string Base = "/pages/api";
 
     public const string Pages = Base + "/pages";
     public const string Contents = Pages + "/getContent";
